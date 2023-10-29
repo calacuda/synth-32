f439721e93a35c1bd90f57043dcabafb7c9c094f
Eoghan West <eowest@gmail.com>
2023-10-28 20:50:26 -0400

    added echo effect

--- src/synth/echo.rs
+++ src/synth/echo.rs
@@ -1,36 +1,25 @@
 use super::Float;
 use crate::SAMPLE_RATE;
-use log::*;
 
 /// fifo buffer of size SAMPLE_RATE that is used to get an "echo" effect.
 pub struct Echo {
-    pub buffer: Vec<Float>,
+    buffer: Vec<Float>,
     pub on: bool,
     volume: Float,
     /// the speed of the echo
     step: usize,
-    pub i: usize,
-    counter: usize,
+    i: usize,
     size: usize,
 }
 
 impl Echo {
     pub fn new() -> Self {
-        info!("making buffer");
         let buffer = (0..(SAMPLE_RATE / 2)).map(|_| 0.0).collect();
-        info!("buffer created");
-        // Buffer::new((SAMPLE_RATE / 2) as usize);
-        // for _ in 0..(SAMPLE_RATE / 2) {
-        //     let _ = buffer.add(0.0);
-        // }
-
         let on = false;
         let volume = 0.5;
         let step = 0;
         let i = 0;
         let size = SAMPLE_RATE as usize / 2;
-        let counter = 0;
-        info!("made Echo state machine");
 
         Self {
             buffer,
@@ -38,7 +27,6 @@ impl Echo {
             volume,
             step,
             i,
-            counter,
             size,
         }
     }
@@ -46,7 +34,6 @@ impl Echo {
     /// designed to accept floats greater then 1.0 and less then or equal to 5.0
     pub fn set_speed(&mut self, speed: Float) {
         self.step = ((1.0 / speed) * self.size as Float) as usize;
-        // info!("speed: {}, size: {}", self.step, self.size);
     }
 
     pub fn set_volume(&mut self, volume: Float) {
@@ -60,7 +47,6 @@ impl Echo {
                 self.buffer[i] = 0.0;
             }
             self.i = 0;
-            self.counter = 0;
         }
 
         self.on = on_off;
