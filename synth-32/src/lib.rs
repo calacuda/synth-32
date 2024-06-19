dac30241301feab8709bee1d3357fc72eb77919f
Eoghan West <eowest@gmail.com>
2024-06-19 07:32:22 -0400

    updated sample rate

--- synth-32/src/lib.rs
+++ synth-32/src/lib.rs
@@ -2,7 +2,7 @@ pub use synth::Float;
 
 pub type Note = Float;
 
-const SAMPLE_RATE: u32 = 44_100;
+const SAMPLE_RATE: u32 = 48_000;
 // pub const SAMPLE_RATE: u32 = 22_050;
 // const SAMPLE_RATE: u32 = 38_750;
 
